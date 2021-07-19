4a9df93d8450d300e277be7025fd104994504563
juandeto <juandeto10@gmail.com>
2021-07-19 13:09:08 -0300

    snake terminado

--- script.js
+++ script.js
@@ -13,23 +13,24 @@ let interval = 0
 
 
 
-// startGame
+// function q se dispara cuando comienza el juego
 function startGame() {
-    //reseteamos las variables
-    currentSnake.forEach(index => squares[index].classList.remove("snake"))
-    squares[appleIndex].classList.remove("apple")
+    //reseteas variables
+    currentSnake.forEach(index => squares[index].classList.remove('snake'))
+    squares[appleIndex].classList.remove('apple')
     clearInterval(interval)
     score = 0
-    //randomApple()
+    randomApple()
     direction = 1
     scoreDisplay.innerText = score
-    intervalTime = 1000 //ms
-
-    //vuelvo al principio
+    intervalTime = 1000
     currentSnake = [2,1,0]
     currentSnake.forEach(index => squares[index].classList.add('snake'))
+    //seteas un intervalo de tiempo para q la funcion movementSnake
+    //se ejecute cada cierto intervalo de tiempo
     interval = setInterval(movementSnake, intervalTime)
-}
+  }
+
 
 
 //moivmiento de la serpiente
@@ -44,34 +45,56 @@ function movementSnake() {
     squares[tail].classList.remove('snake')
     let newSquare;
 
-    if(//cuando choca contra la derecha
-        ((currentSnake[0]+1 % width === 0) && direction === 1)
+    //decide donde se agrega el proximo cuadrado: la cabeza del array
+    if(//cuando choca contra la pared derecha
+        ((currentSnake[0]+1) % width === 0) && (direction === 1)
     ){
-        newSquare = currentSnake[0] - width + 1
-    }else if( //cuando choca contra la izquierda
-        ((currentSnake[0]+1 % width === 0) && direction === -1)
+        newSquare = ((currentSnake[0] - width +1))
+    }else if(//cuando choca contra la izquierda
+        (currentSnake[0] % (width) === 0) && (direction === -1) &&
+        currentSnake[0] === 0
+    ) { 
+        newSquare = ((currentSnake[0] + width-1))
+    }else if(//cuando choca contra el tope
+        (currentSnake[0] + direction  < 0) &&
+        (direction === -width)
     ){
-        newSquare = currentSnake[0] + width - 1
-    }else if( // cuando choca contra arriba
-        (currentSnake[0] - direction < 0) &&
-        direction === -width
-    ){
-        newSquare = currentSnake[0] + squares.length - width
+        newSquare =  (currentSnake[0] + squares.length - width)
     }else if(//cuando choca contra abajo
-        (currentSnake[0] + direction >= squares.length + width) &&
-        direction === width
+        (currentSnake[0] + direction  >= squares.length)&&
+        (direction === width) 
     ){
-        newSquare = currentSnake[0] - squares.length + width
-    }else{
-        newSquare = currentSnake[0] + direction
-    }
-
+        newSquare =  (currentSnake[0] - squares.length + width)
+    }else{//si no choca...
+          newSquare =  (currentSnake[0]  + direction)
+    } 
+    
     currentSnake.unshift(newSquare)
-
-    squares[currentSnake[0]].classList.add('snake')
+  
+        
+  //deals with snake getting apple
+  if(squares[currentSnake[0]].classList.contains('apple')) {
+    squares[currentSnake[0]].classList.remove('apple')
+    squares[tail].classList.add('snake')
+    currentSnake.push(tail)
+    randomApple()
+    score++
+    scoreDisplay.textContent = score * 100
+    clearInterval(interval)
+    intervalTime = intervalTime * speed
+    interval = setInterval(moveOutcomes, intervalTime)
+  }
+  squares[currentSnake[0]].classList.add('snake')
 
 }
 
+//genera nueva manzana cuando es comida
+function randomApple() {
+      do{
+        appleIndex = Math.floor(Math.random() * squares.length)
+      } while(squares[appleIndex].classList.contains('snake')) //making sure apples dont appear on the snake
+      squares[appleIndex].classList.add('apple')
+    }
 
 //direccion de la serpiente
 function control(e) {
